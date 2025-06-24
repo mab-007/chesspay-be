8bd557b712ae3bf8e741cfd689d3027eeced14aa
Amarnath <ambhakat1999@gmail.com>
2025-06-24 09:49:54 +0530

    feat(): completing be routes

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -13,9 +13,29 @@ class AccountRoute {
 
     private initializeRoutes(): void {
         //Get Account Details useing user id
-
+        this.router.get("/:id", async (req, res) => {
+            try {
+                const userId = req.params.id;
+                const accountDetails = await this.accountService.getAccountDetails(userId);
+                res.status(200).json(accountDetails);
+            }
+            catch (error) {
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        });
 
         //Update account balance usering user_id and account_id
+        this.router.put("/:id", async (req, res) => {
+            try {
+                const userId = req.params.id;
+                const { account_id, balance, type } = req.body;
+                const updatedBalance = await this.accountService.updateAccountBalance(userId, account_id, balance, type);
+                res.status(200).json(updatedBalance);
+            } catch (error) {
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+
+        })
 
         
     }
